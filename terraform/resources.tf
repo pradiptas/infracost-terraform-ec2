a1834a6135a5fa9d6cba2a92873e88013c5b8d9f
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2022-04-28 16:07:15 -0700

    backend bucket

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -2,10 +2,16 @@ provider "aws" {
   region	 = "${var.region}"
 }
 
-resource "aws_instance" "ec2-vault-prad-instance" {
+backend "s3" {
+   bucket = "prad-terraform-bucket"
+   key    = "terraform/webapp/terraform.tfstate"
+   region = "us-west-2"
+}
+
+resource "aws_instance" "ec2-prad-instance" {
   ami           = "${var.instance_ami}"
   instance_type = "${var.instance_type}"
   tags = {
-    Name = "ec2-vault-prad-instance"
+    Name = "ec2-prad-instance"
   }
 }
